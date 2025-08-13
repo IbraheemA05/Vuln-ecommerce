16875aeb29b768e7764be26c63669db1a998cb89
windows <windows@whitehacker.noun>
2025-08-13 18:16:53 +0100

    Trobleshooting some bug, routes and auth related issus to the vendor routes

---
+++ server/routes/vendorRoutes.js
@@ -0,0 +1,17 @@
+import { Router } from "express";
+import vendorAuth from "../middleware/Vendorauth.js";
+import * as authController from "../controllers/authController.js";
+import vendorController from "../controllers/vendorController.js";
+
+const router = Router();
+
+router.post("/login", authController.loginVendor);
+router.post("/signup", authController.signupVendor);
+
+router.use(vendorAuth);
+
+router.get("/me", vendorController.getMyVendorProfile);
+router.put("/me", vendorController.updateMyVendorProfile);
+router.delete("/me", vendorController.deleteMyVendorAccount);
+
+export default router;
